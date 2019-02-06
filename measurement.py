53dab1b05f8ab87bfe2c5123d21a852d76ec3db4
DanielHMeyer <danielhmeyer1@gmail.com>
2019-02-05 21:48:01 -0800

    Changed functions so it is easier to set values and assure type safety

--- measurement.py
+++ measurement.py
@@ -2,32 +2,11 @@
 
 from enum import Enum
 
-class MeasurementKeys(Enum):
-    PATIENT_NUMBER = 'NUMBER'
-    PATIENT_AGE = 'AGE'
-    MATURE_OOCYTES = 'MII'
-    CLINIC = 'CLINIC'
-    OOCYTE_NUMBER = 'OOCYTE'
-    POSITION = 'POSITION'
-    FERTILIZED = 'FERTILIZED'
-    DEGENERATED = 'DEGENERATED'
-    NONFERT = 'NONFERT'
-    ABNORMALFERT = 'ABNORMALFERT'
-    D3_GRADE = 'D3GRADE'
-    D3_VERY_GOOD = 'D3VERYGOOD'
-    D3_VERY_BAD = 'D3VERYBAD'
-    D3_GOOD = 'D3GOOD'
-    D3_FAIR = 'D3FAIR'
-    D3_POOR = 'D3POOR'
-    ANY_BLAST = 'ANYBLAST'
-    GOOD_BLAST = 'GOODBLAST'
-    POOR_BLAST = 'POORBLAST'
-    NO_BLAST = 'NOBLAST'
-    BLAST_GRADE = 'BLASTGRADE'
+class PropertyKeys(Enum):
+    ''' A class with keywords of properties '''
     VIDEO_FRAMES = 'VIDEO_FRAMES'
     APPLIED_PRESSURE = 'APPLIED_PRESSURE'
     APPLIED_FORCE = 'APPLIED_FORCE'
-    ROI_COORD = 'ROI_COORD'
     PIPETTE_SIZE_PIXEL = 'PIPETTE_SIZE_PIXEL'
     MANUAL_CONVERSION_FACTOR = 'MANUAL_CONVERSION_FACTOR'
     ZONA_THICKNESS = 'ZONA_THICKNESS'
@@ -37,11 +16,46 @@ class MeasurementKeys(Enum):
     ASPIRATION_DEPTH_ZONA_PIXEL = 'ASPIRATION_DEPTH_ZONA_PIXEL'
     ASPIRATION_DEPTH_ZONA_MECH = 'ASPIRATION_DEPTH_ZONA_MECH'
     PIPETTE_TIP_POSITION = 'PIPETTE_TIP_POSITION'
+
+class OutcomesKeys(Enum):
+    ''' A class with keywords of embryo culture outcomes '''
+    FERTILIZED = 'FERTILIZED'
+    DEGENERATED = 'DEGENERATED'
+    NONFERT = 'NONFERT'
+    ABNORMALFERT = 'ABNORMALFERT'
+    D3GRADE = 'D3GRADE'
+    D3VERYGOOD = 'D3_VERYGOOD'
+    D3VERYBAD = 'D3VERYBAD'
+    D3GOOD = 'D3GOOD'
+    D3FAIR = 'D3FAIR'
+    D3POOR = 'D3POOR'
+    ANYBLAST = 'ANYBLAST'
+    GOODBLAST = 'GOODBLAST'
+    POORBLAST = 'POORBLAST'
+    NOBLAST = 'NOBLAST'
+    BLASTGRADE = 'BLASTGRADE'
+    
+    @classmethod
+    def has_value(cls, value):
+        return any(value == item.value for item in cls)
+
+
+class ParameterKeys(Enum):
+    ''' A class with keywords of the modified Zener model '''
     K0_ZP = 'K0_ZP'
     K1_ZP = 'K1_ZP'
     TAU_ZP = 'TAU_ZP'
     ETA0_ZP = 'ETA0_ZP'
     ETA1_ZP = 'ETA1_ZP'
+    
+class PatientKeys(Enum):
+    ''' A class with keywords of patient information '''
+    PATIENT_NUMBER = 'NUMBER'
+    PATIENT_AGE = 'AGE'
+    MATURE_OOCYTES = 'MII'
+    CLINIC = 'CLINIC'
+    OOCYTE_NUMBER = 'OOCYTE'
+    POSITION = 'POSITION'
 
 class Measurement:
     ''' A container class to store data from aspiration depth measurements '''
@@ -49,8 +63,8 @@ class Measurement:
     _conversion_factor = 1.55       # Conversion factor for pixels to micrometers: 155 pixels per 100 microns
     _pipette_size = 50.0            # pipette diameter [um]
     
-    def __init__(self, patient_number, patient_age, mature_oocytes, clinic, oocyte_number,
-                 measurement_number, position, outcomes={}):
+    def __init__(self, patient_number, patient_age, mature_oocytes, clinic, 
+                 oocyte_number, position, outcomes={}):
         '''
         Initialize an instance of the Measurement object
         
@@ -60,188 +74,120 @@ class Measurement:
             mature_oocytes (int):       number of mature oocytes collected from the patient
             clinic (str):               name of the clinic where measurement was performed
             oocyte_number (int):        number of the oocyte
-            measurement_number (int):   number of the measurement
             position (int):             measurement position (3/9 or 6 o'clock)
             outcomes (dict):            dict with clinical results of embryo culture
         '''
-        for key in MeasurementKeys:
+        self.measurement_data = {}
+        self.set_patient_information(PatientKeys.PATIENT_NUMBER, patient_number)
+        self.set_patient_information(PatientKeys.PATIENT_AGE, patient_age)
+        self.set_patient_information(PatientKeys.MATURE_OOCYTES, mature_oocytes)
+        self.set_patient_information(PatientKeys.CLINIC, clinic)
+        self.set_patient_information(PatientKeys.OOCYTE_NUMBER, oocyte_number)
+        self.set_patient_information(PatientKeys.POSITION, position)
+
+        for key in PropertyKeys:
+            self.measurement_data[key.value] = -1
+        for key in ParameterKeys:
+            self.measurement_data[key.value] = -1
+        for key in OutcomesKeys:
             self.measurement_data[key.value] = -1
 
-        self.measurement_data[
-                MeasurementKeys.PATIENT_NUMBER.value] = patient_number
-        self.measurement_data[MeasurementKeys.PATIENT_AGE.value] = patient_age
-        self.measurement_data[
-                MeasurementKeys.MATURE_OOCYTES.value] = mature_oocytes
-        self.measurement_data[MeasurementKeys.CLINIC.value] = clinic
-        self.measurement_data[
-                MeasurementKeys.OOCYTE_NUMBER.value] = oocyte_number
-        self.measurement_data[MeasurementKeys.POSITION.value] = position
-
         if outcomes:
-            self.measurement_data.update(outcomes)
+            for key, value in outcomes.items():
+                if OutcomesKeys.has_value(key):
+                    self.set_outcome(OutcomesKeys[key], value)
     
-    def set_outcomes(self, outcomes):
+    def set_patient_information(self, key, info):
+        '''
+        Set information of a patient.
+        
+        args:
+            key (PatientKeys): an attribute of the PatientKeys class
+            info (str or int): patient information
+        '''
+        if not isinstance(key, PatientKeys):
+            raise TypeError('Key is invalid. Expected {},' 
+                    ' but got {} instead.'.format(PatientKeys, type(key)))
+        if (key == PatientKeys.CLINIC):
+            if not isinstance(info, str):
+                raise TypeError('Info is invalid. Expected {},'
+                                ' but got {} instead.'.format(str, type(info)))
+        else:
+            if not isinstance(info, int):
+                raise TypeError('Info is invalid. Expected {},'
+                                ' but got {} instead.'.format(
+                        int, type(info)))
+        self.measurement_data[key.value] = info
+    
+    def set_outcome(self, key, outcome):
         ''' 
-        Set the development outcomes of the oocytes.
+        Set a development outcome of an oocyte.
         
         args:
-            outcomes (dict): dict with developmental outcomes
+            key (OutcomeKey): an attribute of the OutcomeKey class
+            outcome (str or int): value of the outcome
         '''
-        self.measurement_data.update(outcomes)
+        if not isinstance(key, OutcomesKeys):
+            raise TypeError('Key is invalid. Expected {},'
+                            ' but got {} instead.'.format(
+                            OutcomesKeys, type(key)))
+        if ((key == OutcomesKeys.BLASTGRADE) | (key == OutcomesKeys.D3GRADE)):
+            if not isinstance(outcome, str):
+                raise TypeError('Outcome is invalid. Expected {},'
+                                ' but got {} instead.'.format(
+                        str, type(outcome)))
+        else:
+            if not isinstance(outcome, int):
+                raise TypeError('Outcome is invalid. Expected {},'
+                                ' but got {} instead.'.format(
+                        int, type(outcome)))
+        self.measurement_data[key.value] = outcome
     
-    def set_video_frames(self, video_frames):
+    def set_property(self, key, measurement_property):
         '''
-        Set the video_frames of the video.
+        Set a property of a measurement.
         
         args:
-            video_frames (list of array): a list of images
+            key (PropertyKey): an attribute of the PropertyKey class
+            property (list, float): the value of the property
         '''
-        self.measurement_data[
-                MeasurementKeys.VIDEO_FRAMES.value] = video_frames
-    
-    def set_applied_pressure(self, pressure):
+        if not isinstance(key, PropertyKeys):
+            raise TypeError('Key is invalid. Expected {},'
+                            ' but got {} instead.'.format(
+                    PropertyKeys, type(key)))
+        if ((key == PropertyKeys.VIDEO_FRAMES) | 
+            (key == PropertyKeys.ASPIRATION_DEPTH_ZONA_MECH) |
+            (key == PropertyKeys.ASPIRATION_DEPTH_ZONA_PIXEL) |
+            (key == PropertyKeys.TIME)):
+            if not isinstance(measurement_property, list):
+                raise TypeError('Property is invalid. Expected {},'
+                                ' but got {} instead.'.format(list,
+                            type(measurement_property)))
+        else:
+            if not isinstance(measurement_property, float):
+                raise TypeError('Property is invalid. Expected {},'
+                                ' but got {} instead.'.format(float,
+                                type(measurement_property)))
+        
+        self.measurement_data[key.value] = property
+       
+    def set_model_parameter(self, key, parameter):
         '''
-        Set the applied pressure.
-        Calculate and set the force applied to the oocyte.
+        Set a model parameter of the modified Zener model.
         
         args:
-            pressure (float):     applied pressure in [psi]
-              
-        Formula: F = P*A (Force = Pressure * Area)
+            key (ParameterKeys): an attribute of the ParameterKeys class
+            parameter (float): value of the model parameter
         '''
-        self.measurement_data[
-                MeasurementKeys.APPLIED_PRESSURE.value] = pressure
-        PSI_TO_NM2 = 6894.76   # conversion factor from [psi] to [N/m^2]
-        UM_TO_M = 1e-6       # conversion factor from [um] to [m]
-        force = (pressure * PSI_TO_NM2 
-                    * 3.1415 * (self._pipette_size/2.0*UM_TO_M)**2)
-        self.set_applied_force(force)
-        
-    def set_applied_force(self, force):
-        '''
-        Set the applied force.
-        
-        args:
-            force (float): applied force
-        '''
-        self.measurement_data[MeasurementKeys.APPLIED_FORCE.value] = force
-        
-    def set_roi_coordinates(self, coord):
-        '''
-        Set the coordinates of the region of interest.
-        
-        args:
-            coord (tuple): top left corner coordinates, width and height
-        '''
-        self.measurement_data[MeasurementKeys.ROI_COORD.value] = coord
-        
-    def set_pipette_size(self, pipette_size):
-        '''
-        Set the pipette size in pixels. Calculate and set the conversion factor.
-        
-        args:
-            pipette_size (int): pipette size [pixels]
-        '''
-        self.measurement_data[
-                MeasurementKeys.PIPETTE_SIZE_PIXEL.value] = pipette_size
-        conversion_factor = pipette_size/self._pipette_size
-        self.set_manual_conversion_factor(conversion_factor)
-        
-    def set_manual_conversion_factor(self, conversion_factor):
-        '''
-        Set the manual conversion factor.
-        
-        args:
-            conversion_factor (float): manually determined conversion factor
-        '''
-        self.measurement_data[
-                MeasurementKeys.MANUAL_CONVERSION_FACTOR.value
-                ] = conversion_factor
-        
-    def set_zona_thickness(self, zona_thickness):
-        '''
-        Set the zona thickness.
-        
-        args:
-            zona_thickness (float): thickness of the zona pellucida
-        '''
-        self.measurement_data[
-                MeasurementKeys.ZONA_THICKNESS.value] = zona_thickness
-        
-    def set_perivitelline_space(self, perivitelline_space):
-        '''
-        Set the perivitelline space.
-        
-        args:
-            perivitelline_space (float): size of the perivitelline space
-        '''
-        self.measurement_data[
-                MeasurementKeys.PERIVITELLINE_SPACE.value
-                ] = perivitelline_space
-    
-    def set_zona_position(self, zona_position):
-        '''
-        Set the zona postion.
-        
-        args:
-            zona_position (int): initial position of the zona pellucida in the image
-        '''
-        self.measurement_data[
-                MeasurementKeys.ZONA_POSITION.value] = zona_position
-    
-    def set_time(self, time):
-        '''
-        Set the time vector.
-        
-        args:
-            time (array of float): time vector [s]
-        '''
-        self.measurement_data[MeasurementKeys.TIME.value] = time
-        
-    def set_aspiration_depth_zona_pixel(self, aspiration_depth):
-        '''
-        Set the aspiration depth in pixels.
-        
-        args:
-            aspiration_depth (list of int): aspiration depth of each image [pixels]
-        '''
-        self.measurement_data[
-                MeasurementKeys.ASPIRATION_DEPTH_ZONA_PIXEL.value
-                ] = aspiration_depth
-        
-    def set_aspiration_depth_zona_mech(self, aspiration_depth):
-        '''
-        Set the aspiration depth in um.
-        
-        args:
-            aspiration_depth (list of float): aspiration depth of each image [um]
-        '''
-        self.measurement_data[
-                MeasurementKeys.ASPIRATION_DEPTH_ZONA_MECH.value
-                ] = aspiration_depth
-        
-    def set_pipette_tip_position(self, tip_position):
-        '''
-        Set pipette tip position.
-        
-        args:
-            tip_position (int): position of the pipette tip in the first image
-        '''
-        self.measurement_data[
-                MeasurementKeys.PIPETTE_TIP_POSITION.value] = tip_position
-        
-    def set_model_parameters(self, k0, k1, eta0, eta1, tau):
-        '''
-        Set the model parameters of the modified Zener model.
-        
-        args:
-            k0, k1, eta0, eta1, tau (float): model parameters
-        '''
-        self.measurement_data[MeasurementKeys.K0_ZP.value] = k0
-        self.measurement_data[MeasurementKeys.K1_ZP.value] = k1
-        self.measurement_data[MeasurementKeys.ETA0_ZP.value] = eta0
-        self.measurement_data[MeasurementKeys.ETA1_ZP.value] = eta1
-        self.measurement_data[MeasurementKeys.TAU_ZP.value] = tau
+        if not isinstance(key, ParameterKeys):
+            raise TypeError('Key is invalid. Expected {},'
+                            ' but got {} instead.'.format(
+                    ParameterKeys, type(key)))
+        if not isinstance(parameter, float):
+            raise TypeError('Parameter is invalid. Expected {},'
+                            ' but got {} instead.'.format(
+                    float, type(parameter)))
+        self.measurement_data[key.value] = parameter
      
         
 if __name__ == '__main__':
